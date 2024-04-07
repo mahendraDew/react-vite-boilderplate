60f0addad43f461c449d1a35a01a70b4a8a20e30
“MahendraDew” <mahendradewangan195@gmail.com>
2024-04-07 18:48:11 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        
+      <h2>Simple vite react boilerplate code for testing</h2>
+     
+      </div>
+    </>
+  )
+}
+
+export default App
